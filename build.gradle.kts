0a3c9cc3bb15bfdfe40cd274f3413551d7139d04
Ondřej Milenovský <wanucha@seznam.cz>
2025-04-20 18:59:53 +0200

    Small build update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     java
     kotlin("jvm") version "2.0.0"
@@ -22,7 +24,7 @@ dependencies {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
 }
 
 tasks.register<Jar>("fatJar") {
